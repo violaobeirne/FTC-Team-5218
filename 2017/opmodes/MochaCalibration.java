ac79f363942150e9c3789cd86a3996015dad3fb0
izzielau <izzielau@rocknrollrobots25.com>
2016-12-27 10:35:55 -0800

    Changes to configuration file from competition.

--- 2017/opmodes/MochaCalibration.java
+++ 2017/opmodes/MochaCalibration.java
@@ -7,4 +7,12 @@ public class MochaCalibration {
 
     public static final int TICKS_PER_DEGREE = 18;
     public static final int TICKS_PER_INCH = 79;
+
+    public static final double SHOOTER_AUTO_VORTEX = 0.2;
+    public static final double SHOOTER_AUTO_CORNER = 0.235;
+
+    public static final double SHOOTER_LOW_A = 0.5;
+    public static final double SHOOTER_MED_Y = 0.65;
+    public static final double SHOOTER_HIGH_X = 0.85;
+
 }
