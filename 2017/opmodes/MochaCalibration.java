da57651fd35e73b6c20dbdb4d401be024e60b530
izzielau <izzielau@rocknrollrobots25.com>
2016-12-28 21:19:07 -0800

    Changes to shooter speed configurations.

--- 2017/opmodes/MochaCalibration.java
+++ 2017/opmodes/MochaCalibration.java
@@ -12,6 +12,7 @@ public class MochaCalibration {
     public static final double SHOOTER_AUTO_CORNER = 0.235;
 
     public static final double SHOOTER_LOW_A = 0.5;
-    public static final double SHOOTER_MED_Y = 0.65;
-    public static final double SHOOTER_HIGH_X = 0.85;
+    public static final double SHOOTER_MED_Y = 0.4;
+    public static final double SHOOTER_HIGH_X = 0.425;
+
 }
