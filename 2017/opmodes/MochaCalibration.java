ef6040290b859925a83bb5fe1d4fb4942c8dbd16
izzielau <izzielau@rocknrollrobots25.com>
2017-01-02 17:15:39 -0800

    Changes to shooter speeds.

--- 2017/opmodes/MochaCalibration.java
+++ 2017/opmodes/MochaCalibration.java
@@ -5,16 +5,22 @@ package opmodes;
  */
 public class MochaCalibration {
 
-    public static final int TICKS_PER_DEGREE = 18;
+    public static final int TICKS_PER_DEGREE = 17;
     public static final int TICKS_PER_INCH = 79;
 
-    public static final double SHOOTER_AUTO_VORTEX = 0.2;
-    public static final double SHOOTER_AUTO_CORNER = 0.235;
+    public static final double SHOOTER_AUTO_VORTEX = 0.225;
+    public static final double SHOOTER_AUTO_CORNER = 0.5;
 
-    public static final double SHOOTER_LOW_A = 0.5;
-    public static final double SHOOTER_MED_Y = 0.4;
-    public static final double SHOOTER_HIGH_X = 0.425;
+    public static final double SHOOTER_Y = 0.5;
+    public static final double SHOOTER_B = 0.45;
+    public static final double SHOOTER_A = 0.425;
+    public static final double SHOOTER_X = 0.4  ;
 
-    public static final int LIGHT_MINIMUM = 0;
-    public static final int LIGHT_MAXIMUM = 0;
+    public static final double LIGHT_MINIMUM = 1.35;
+    public static final double LIGHT_MAXIMUM = 2.01;
+
+    public static final double TURN_SPEED = 0.2;
+    public static final double MOVE_SPEED = 0.65;
+    public static final double LINE_SPEED = 0.1;
+
 }
