e95084a642e4ac8dc350d9ec8ecc2dea08e2e0ec
Craig MacFarlane <camacfarl@gmail.com>
2017-01-26 12:53:46 -0800

    Checkpoint white line edge detection.

--- 2017/test/LameingoAlignWithLineTest.java
+++ 2017/test/LameingoAlignWithLineTest.java
@@ -9,27 +9,28 @@ import com.qualcomm.robotcore.util.RobotLog;
 import team25core.AlignWithWhiteLineTask;
 import team25core.LightSensorCriteria;
 import team25core.MonitorMotorTask;
+import team25core.OpticalDistanceSensorCriteria;
 import team25core.Robot;
 import team25core.RobotEvent;
 import team25core.TwoWheelDirectDrivetrain;
 
 @Autonomous(name = "TEST AlignWithLine", group = "AutoTest")
-@Disabled
 public class LameingoAlignWithLineTest extends Robot {
 
     private DcMotor right;
     private DcMotor left;
-    private LightSensorCriteria leftSeeWhite;
-    private LightSensorCriteria leftSeeBlack;
-    private LightSensorCriteria rightSeeWhite;
-    private LightSensorCriteria rightSeeBlack;
+    private OpticalDistanceSensorCriteria leftSeeWhite;
+    private OpticalDistanceSensorCriteria leftSeeBlack;
+    private OpticalDistanceSensorCriteria rightSeeWhite;
+    private OpticalDistanceSensorCriteria rightSeeBlack;
     private TwoWheelDirectDrivetrain drivetrain;
     private LightSensor leftLight;
     private LightSensor rightLight;
     private AlignWithWhiteLineTask alignTask;
 
-    private static final double LIGHT_MIN = 1.35;
-    private static final double LIGHT_MAX = 2.01;
+    private static final double LIGHT_MIN = 0.7;
+    private static final double LIGHT_MAX = 4.0;
+    private static final double PIVOT_MOD = 4.0;
 
     @Override
     public void handleEvent(RobotEvent e)
@@ -41,17 +42,24 @@ public class LameingoAlignWithLineTest extends Robot {
     @Override
     public void init()
     {
-        leftLight = hardwareMap.lightSensor.get("left");
-        rightLight = hardwareMap.lightSensor.get("right");
-        right = hardwareMap.dcMotor.get("right");
-        left = hardwareMap.dcMotor.get("left");
+        leftLight = hardwareMap.opticalDistanceSensor.get("left");
+        rightLight = hardwareMap.opticalDistanceSensor.get("right");
+        right = hardwareMap.dcMotor.get("rightMotor");
+        left = hardwareMap.dcMotor.get("leftMotor");
 
-        leftSeeWhite = new LightSensorCriteria(leftLight, LightSensorCriteria.LightPolarity.WHITE, LIGHT_MIN, LIGHT_MAX);
-        leftSeeBlack = new LightSensorCriteria(leftLight, LightSensorCriteria.LightPolarity.BLACK, LIGHT_MIN, LIGHT_MAX);
-        rightSeeWhite = new LightSensorCriteria(rightLight, LightSensorCriteria.LightPolarity.WHITE, LIGHT_MIN, LIGHT_MAX);
-        rightSeeBlack = new LightSensorCriteria(rightLight, LightSensorCriteria.LightPolarity.BLACK, LIGHT_MIN, LIGHT_MAX);
+        leftSeeWhite = new OpticalDistanceSensorCriteria(leftLight, OpticalDistanceSensorCriteria.LightPolarity.WHITE, LIGHT_MIN, LIGHT_MAX);
+        leftSeeBlack = new OpticalDistanceSensorCriteria(leftLight, OpticalDistanceSensorCriteria.LightPolarity.BLACK, LIGHT_MIN, LIGHT_MAX);
+        rightSeeWhite = new OpticalDistanceSensorCriteria(rightLight, OpticalDistanceSensorCriteria.LightPolarity.WHITE, LIGHT_MIN, LIGHT_MAX);
+        rightSeeBlack = new OpticalDistanceSensorCriteria(rightLight, OpticalDistanceSensorCriteria.LightPolarity.BLACK, LIGHT_MIN, LIGHT_MAX);
 
-        drivetrain = new TwoWheelDirectDrivetrain(0, right, left);
+        leftSeeBlack.setName("Left");
+        leftSeeWhite.setName("Left");
+        rightSeeBlack.setName("Right");
+        rightSeeWhite.setName("Right");
+
+        drivetrain = new TwoWheelDirectDrivetrain(0, PIVOT_MOD, right, left);
+        drivetrain.resetEncoders();
+        drivetrain.encodersOn();
 
         alignTask = new AlignWithWhiteLineTask(this, drivetrain, leftSeeBlack, leftSeeWhite, rightSeeBlack, rightSeeWhite);
     }
