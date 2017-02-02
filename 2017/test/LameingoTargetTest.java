1202ab433075f396018dc01ca2ac9e53b858555f
Craig MacFarlane <camacfarl@gmail.com>
2017-02-02 14:47:11 -0800

    Checkpoint commit for target finding.

--- 2017/test/LameingoTargetTest.java
+++ 2017/test/LameingoTargetTest.java
@@ -2,6 +2,7 @@ package test;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import team25core.Robot;
 import team25core.RobotEvent;
@@ -16,10 +17,20 @@ import team25core.Robot_TwoWheelDrive;
 @Autonomous(name = "TEST Target", group = "AutoTest")
 public class LameingoTargetTest extends Robot {
 
+    enum TargetState {
+        FIND_TARGET,
+        LOST_TARGET,
+        INITIAL_APPROACH,
+        AT_TARGET,
+        ALIGNED,
+    };
+
     Robot_Navigation nav;
     Robot_TwoWheelDrive drive;
     DcMotor leftMotor;
     DcMotor rightMotor;
+    TargetState state;
+    double bearing;
 
     @Override
     public void handleEvent(RobotEvent e) {
@@ -29,15 +40,23 @@ public class LameingoTargetTest extends Robot {
     @Override
     public void init()
     {
-        drive = new Robot_TwoWheelDrive();
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        drive = new Robot_TwoWheelDrive(rightMotor, leftMotor);
         drive.initDrive(this);
         nav = new Robot_Navigation();
         nav.initVuforia(this, drive);
+
+        nav.activateTracking();
     }
 
     @Override
     public void start()
     {
+        state = TargetState.FIND_TARGET;
     }
 
     @Override
@@ -45,8 +64,59 @@ public class LameingoTargetTest extends Robot {
     {
         super.loop();
 
-        telemetry.addData("Target visible", nav.targetsAreVisible());
+        nav.targetsAreVisible();
+        // nav.addNavTelemetry();
 
-        telemetry.update();
+        switch (state) {
+        case FIND_TARGET:
+            if (nav.targetsAreVisible()) {
+                drive.rotateRobot(0.0);
+                nav.addNavTelemetry();
+                // state = TargetState.INITIAL_APPROACH;
+            } else {
+                this.telemetry.addData("Rotating...", "");
+                drive.rotateRobot(0.06);
+            }
+            break;
+        case LOST_TARGET:
+            if (nav.targetsAreVisible()) {
+                state = TargetState.INITIAL_APPROACH;
+            } else if (nav.getRelativeBearing() > 0) {
+                drive.rotateRobot(-0.10);
+            } else {
+                drive.rotateRobot(0.10);
+            }
+            break;
+        case INITIAL_APPROACH:
+            if (nav.targetsAreVisible()) {
+                if (!nav.cruiseControl(200)) {
+                    drive.moveRobot();
+                } else {
+                    state = TargetState.AT_TARGET;
+                    bearing = nav.getRelativeBearing();
+                    drive.moveRobot(0.0, 0.0, 0.0);
+                }
+            } else {
+                RobotLog.i("Lost target %f", nav.getRelativeBearing());
+                state = TargetState.LOST_TARGET;
+            }
+            break;
+        case AT_TARGET:
+            RobotLog.i("Entering state AT_TARGET");
+            if ((nav.getRobotBearing() < 3) && (nav.getRobotBearing() > -3)) {
+                state = TargetState.ALIGNED;
+            } else if (nav.getRobotBearing() > 0) {
+                drive.rotateRobot(0.10);
+            } else {
+                drive.rotateRobot(-0.10);
+            }
+            break;
+        case ALIGNED:
+            RobotLog.i("Entering state ALIGNED");
+            drive.moveRobot(0.0, 0.0, 0.0);
+            break;
+        }
+
+
     }
 }
