5e59c9c9f70fd0d9a9696c2dd1c68c67297352e6
izzielau <izzielau@rocknrollrobots25.com>
2017-02-08 16:34:21 -0800

    Addition of cap ball deadman task in teleop.

--- 2017/opmodes/MochaTeleop.java
+++ 2017/opmodes/MochaTeleop.java
@@ -39,12 +39,11 @@ public class MochaTeleop extends Robot {
     private DcMotor shooterLeft;
     private DcMotor shooterRight;
     private DcMotor sbod;
+    private DcMotor capball;
     private Servo beacon;
     private DeviceInterfaceModule interfaceModule;
     private LightSensor one;
     private LightSensor two;
-
-    private PersistentTelemetryTask ptt;
 
     @Override
     public void init()
@@ -63,26 +62,27 @@ public class MochaTeleop extends Robot {
         frontLeft = hardwareMap.dcMotor.get("motorFL");
         backRight = hardwareMap.dcMotor.get("motorBR");
         backLeft = hardwareMap.dcMotor.get("motorBL");
-
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // Flywheels.
         shooterLeft = hardwareMap.dcMotor.get("shooterLeft");
         shooterRight = hardwareMap.dcMotor.get("shooterRight");
-
         shooterLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         shooterLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         shooterRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         shooterRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Hook.
+        // SBOD.
         sbod = hardwareMap.dcMotor.get("brush");
 
         // Servo.
         beacon = hardwareMap.servo.get("beacon");
 
+        // Cap ball.
+        capball = hardwareMap.dcMotor.get("capball");
     }
 
     @Override
@@ -92,8 +92,6 @@ public class MochaTeleop extends Robot {
 
     @Override
     public void start() {
-        super.start();
-
         /* DRIVER ONE */
         // Four motor drive.
         final FourWheelDriveTask drive = new FourWheelDriveTask(this, frontLeft, frontRight, backLeft, backRight);
@@ -105,11 +103,6 @@ public class MochaTeleop extends Robot {
         DeadmanMotorTask dispense = new DeadmanMotorTask(this, sbod, -BRUSH_SPEED, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.RIGHT_TRIGGER);
         addTask(dispense);
 
-        DeadmanMotorTask collectSlow = new DeadmanMotorTask(this, sbod, 0.5 * BRUSH_SPEED, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.LEFT_BUMPER);
-        addTask(collectSlow);
-        DeadmanMotorTask dispenseSlow = new DeadmanMotorTask(this, sbod, -0.5 * BRUSH_SPEED, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.LEFT_TRIGGER);
-        addTask(dispenseSlow);
-
         // Shooters
         DeadmanMotorTask shootLeftY = new DeadmanMotorTask(this, shooterLeft, SHOOTER_Y, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_Y);
         addTask(shootLeftY);
@@ -119,14 +112,16 @@ public class MochaTeleop extends Robot {
         addTask(shootLeftB);
         DeadmanMotorTask shootRightB = new DeadmanMotorTask(this, shooterRight, -SHOOTER_B, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_B);
         addTask(shootRightB);
-        DeadmanMotorTask shootLeftA = new DeadmanMotorTask(this, shooterLeft, SHOOTER_A, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_A);
-        addTask(shootLeftA);
-        DeadmanMotorTask shootRightA = new DeadmanMotorTask(this, shooterRight, -SHOOTER_A, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_A);
-        addTask(shootRightA);
-        DeadmanMotorTask shootLeftX = new DeadmanMotorTask(this, shooterLeft, SHOOTER_X, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_X);
-        addTask(shootLeftX);
-        DeadmanMotorTask shootRightX = new DeadmanMotorTask(this, shooterRight, -SHOOTER_X, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_X);
-        addTask(shootRightX);
+        DeadmanMotorTask shootLeftBackwards = new DeadmanMotorTask(this, shooterLeft, -0.251, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_X);
+        addTask(shootLeftBackwards);
+        DeadmanMotorTask shootRightBackwards = new DeadmanMotorTask(this, shooterRight, 0.251, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.BUTTON_X);
+        addTask(shootRightBackwards);
+
+        /* DRIVER TWO */
+        DeadmanMotorTask capBallUp = new DeadmanMotorTask(this, capball, 0.8, GamepadTask.GamepadNumber.GAMEPAD_1, DeadmanMotorTask.DeadmanButton.LEFT_BUMPER);
+        addTask(capBallUp);
+        DeadmanMotorTask capBallDown = new DeadmanMotorTask(this, capball, -0.8, GamepadTask.GamepadNumber.GAMEPAD_1, DeadmanMotorTask.DeadmanButton.LEFT_TRIGGER);
+        addTask(capBallDown);
 
         this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1) {
             public void handleEvent(RobotEvent e) {
@@ -142,42 +137,10 @@ public class MochaTeleop extends Robot {
                     beacon.setPosition(0.5);
                 } else if (event.kind == EventKind.BUTTON_B_DOWN) {
                     drive.slowDown(true);
-                    drive.slowDown(0.35);
+                    drive.slowDown(0.4);
                 } else if (event.kind == EventKind.BUTTON_A_DOWN) {
                     drive.slowDown(true);
                     drive.slowDown(1.0);
-                }
-            }
-        });
-
-        addTask(new LimitSwitchTask(this, interfaceModule, 0) {
-            @Override
-            public void handleEvent(RobotEvent e)
-            {
-                LimitSwitchEvent event = (LimitSwitchEvent)e;
-
-                if (event.kind == EventKind.CLOSED) {
-                    telemetry.addData("Status (1): ", "closed");
-                } else if (event.kind == EventKind.OPEN) {
-                    telemetry.addData("Status (1): ", "open");
-                } else {
-                    telemetry.addData("Status (1): ", "unknown");
-                }
-            }
-        });
-
-        addTask(new LimitSwitchTask(this, interfaceModule, 1) {
-            @Override
-            public void handleEvent(RobotEvent e)
-            {
-                LimitSwitchEvent event = (LimitSwitchEvent)e;
-
-                if (event.kind == EventKind.CLOSED) {
-                    telemetry.addData("Status (2): ", "closed");
-                } else if (event.kind == EventKind.OPEN) {
-                    telemetry.addData("Status (2): ", "open");
-                } else {
-                    telemetry.addData("Status (2): ", "unknown");
                 }
             }
         });
