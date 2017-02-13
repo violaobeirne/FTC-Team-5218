cad5a73a04a8cdfc2f85ee8eb2c636366354d7a4
Craig MacFarlane <camacfarl@gmail.com>
2017-02-13 13:56:30 -0800

    Changes to use new color sensor driver.

--- 2017/test/ColorSensorTest.java
+++ 2017/test/ColorSensorTest.java
@@ -1,14 +1,17 @@
 package test;
 
+import com.qualcomm.hardware.ams.AMSColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.DigitalChannelController;
+import com.qualcomm.robotcore.hardware.I2cDeviceImpl;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.ServiceConfigurationError;
 
+import team25core.AMSColorSensorImproved;
 import team25core.ColorSensorTask;
 import team25core.Robot;
 import team25core.RobotEvent;
@@ -20,8 +23,7 @@ import team25core.RobotEvent;
 public class ColorSensorTest extends Robot {
 
     private DeviceInterfaceModule interfaceModule;
-    // private ColorSensor colorLeft;
-    private ColorSensor colorRight;
+    private AMSColorSensorImproved colorRight;
 
     @Override
     public void handleEvent(RobotEvent e) {
@@ -31,8 +33,7 @@ public class ColorSensorTest extends Robot {
     @Override
     public void init() {
         interfaceModule = hardwareMap.deviceInterfaceModule.get("interface");
-        // colorLeft = hardwareMap.colorSensor.get("colorLeft");
-        colorRight = hardwareMap.colorSensor.get("color");
+        colorRight = AMSColorSensorImproved.create(AMSColorSensor.Parameters.createForAdaFruit(), new I2cDeviceImpl(interfaceModule, 0));
 
         interfaceModule.setDigitalChannelMode(0, DigitalChannelController.Mode.OUTPUT);
         interfaceModule.setDigitalChannelState(0, false);
@@ -41,20 +42,8 @@ public class ColorSensorTest extends Robot {
     @Override
     public void loop() {
 
-        // int leftRed = colorLeft.red();
-        // int leftBlue = colorLeft.blue();
         int rightRed = colorRight.red();
         int rightBlue = colorRight.blue();
-
-        /*
-        if (leftRed > leftBlue) {
-            telemetry.addData("STATE (L): ", "Red");
-        } else if (leftRed < leftBlue) {
-            telemetry.addData("STATE (L): ", "Blue");
-        } else {
-            telemetry.addData("STATE (L): ", "Unknown");
-        }
-        */
 
         if (rightRed > rightBlue) {
             telemetry.addData("STATE (R): ", "Red");
@@ -63,9 +52,6 @@ public class ColorSensorTest extends Robot {
         } else {
             telemetry.addData("STATE (R): ", "Unknown");
         }
-
-        // telemetry.addData("LEFT (R): ", leftRed);
-        // telemetry.addData("LEFT (B): ", leftBlue);
 
         telemetry.addData("RIGHT (R): ", rightRed);
         telemetry.addData("RIGHT (B): ", rightBlue);
