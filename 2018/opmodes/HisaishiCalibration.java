d4efe7af2095d1378f4a4723cfb3fa61b6d5304f
izzielau <izzielau@rocknrollrobots25.com>
2018-01-16 21:40:49 -0800

    Updated constants for high-torque servos.

--- 2018/opmodes/HisaishiCalibration.java
+++ 2018/opmodes/HisaishiCalibration.java
@@ -13,14 +13,14 @@ public class HisaishiCalibration {
     // glyph mechanisms
     public static final double GLYPH_LIFT_ELEVATOR_POWER = 0.6;
     public static final double GLYPH_DROP_ELEVATOR_POWER = 0.1;
-    public static final double GLYPH_OPEN_LEFT_POSITION = 0.3;
-    public static final double GLYPH_OPEN_RIGHT_POSITION = 0.7;
-    public static final double GLYPH_CLOSE_LEFT_POSITION = 0;
-    public static final double GLYPH_CLOSE_RIGHT_POSITION = 1.0;
-    public static final double GLYPH_STOW_LEFT_POSITION = 1.0;
-    public static final double GLYPH_STOW_RIGHT_POSITION = 0;
-    public static final double GLYPH_HALF_OPEN_LEFT_POSITION = 0.25;
-    public static final double GLYPH_HALF_OPEN_RIGHT_POSITION = 0.75;
+    public static final double GLYPH_OPEN_LEFT_POSITION = 188.0/256.0;
+    public static final double GLYPH_OPEN_RIGHT_POSITION = 98.0/256.0;
+    public static final double GLYPH_CLOSE_LEFT_POSITION = 95.0/256.0;
+    public static final double GLYPH_CLOSE_RIGHT_POSITION = 195.0/256.0;
+    public static final double GLYPH_STOW_LEFT_POSITION = 248.0/256.0;
+    public static final double GLYPH_STOW_RIGHT_POSITION = 46.0/256.0;
+    public static final double GLYPH_HALF_OPEN_LEFT_POSITION = 134.0/256.0;
+    public static final double GLYPH_HALF_OPEN_RIGHT_POSITION = 158.0/256.0;
     public static final double GLYPH_LEFT_SLIDE_POWER = -1.0;
     public static final double GLYPH_RIGHT_SLIDE_POWER = 1.0;
     public static final double GLYPH_STOP_SLIDE_POWER = 0.0;
@@ -39,8 +39,8 @@ public class HisaishiCalibration {
     public static final int RED_BLUE_UPPER = 0xff;
 
     // relic mechanisms
-    public static final double RELIC_GRABBER_OPEN_POSITION = 82.0/256.0;
-    public static final double RELIC_GRABBER_CLOSE_POSITION = 52.0/256.0;
+    public static final double RELIC_GRABBER_OPEN_POSITION = 1.0;
+    public static final double RELIC_GRABBER_CLOSE_POSITION = 0;
     public static final double RELIC_EXTENDER_POWER = 0.5;
     public static final double RELIC_CASTER_POWER = 0.55;
 }
