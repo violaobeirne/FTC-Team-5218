361a021077bd2571a8bd5bc3b86e3cf5c6523c92
h0llycarter <hollycarter@rocknrollrobots25.com>
2019-11-01 17:50:15 -0700

    Teleop updates.

--- 2020/opmodes/MozartLM0Teleop.java
+++ 2020/opmodes/MozartLM0Teleop.java
@@ -20,10 +20,13 @@ public class MozartLM0Teleop extends Robot {
     /* GAMEPAD 2
     // linear lift up (right bumper) down (right trigger)
     // active wheel intake in (A) out (B)
+    // lazy susan --
+        -- also change servo to != continuous, instead just standard
      */
 
     /* GAMEPAD 1
     // drivetrain
+    // foundation arms stow (right bumper) and down (right trigger)
     // slow mode!
      */
     private DcMotor frontLeft;
@@ -36,6 +39,9 @@ public class MozartLM0Teleop extends Robot {
     private DcMotor rightIntake;
     private DcMotor lift;
     private Servo claw;
+    private DcMotor leftArm;
+    private DcMotor rightArm;
+    private Servo susan;
 
     public void handleEvent (RobotEvent e) {
 
@@ -50,10 +56,13 @@ public class MozartLM0Teleop extends Robot {
         TankMechanumControlScheme scheme = new TankMechanumControlScheme(gamepad1);
         drivetrain = new MechanumGearedDrivetrain(60, frontLeft,frontRight, backLeft, backRight);
         driveTask = new TeleopDriveTask(this, scheme, frontLeft, frontRight, backLeft, backRight);
-        leftIntake = hardwareMap.get(DcMotor.class, "leftIntake");
+        leftIntake = hardwareMap.get(DcMotor.class, "leftIntake"); //wheel intake
         rightIntake = hardwareMap.get(DcMotor.class, "rightIntake");
-        lift = hardwareMap.dcMotor.get("lift");
-        claw = hardwareMap.servo.get("claw");
+        leftArm = hardwareMap.get(DcMotor.class, "leftArm"); //foundation mover
+        rightArm = hardwareMap.get(DcMotor.class, "rightArm");
+        lift = hardwareMap.dcMotor.get("lift"); //lift
+        claw = hardwareMap.servo.get("claw"); //claw
+        susan = hardwareMap.servo.get("susan"); //lazy susan
         // GamepadTask gamepad = new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_2);
         // addTask(gamepad);
     }
@@ -64,6 +73,7 @@ public class MozartLM0Teleop extends Robot {
         this.addTask(driveTask);
 
         // GAMEPAD 2
+        //lift
         DeadmanMotorTask liftUp = new DeadmanMotorTask(this, lift, HisaishiCalibration.LIFT_UP, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.RIGHT_BUMPER);
         addTask(liftUp);
         DeadmanMotorTask liftDown = new DeadmanMotorTask(this, lift, HisaishiCalibration.LIFT_DOWN, GamepadTask.GamepadNumber.GAMEPAD_2, DeadmanMotorTask.DeadmanButton.RIGHT_TRIGGER);
@@ -72,15 +82,58 @@ public class MozartLM0Teleop extends Robot {
         this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_2) {
             public void handleEvent(RobotEvent e) {
                 GamepadEvent event = (GamepadEvent) e;
-                if (event.kind == EventKind.LEFT_BUMPER_DOWN) {
-                    leftIntake.setPower(HisaishiCalibration.INTAKE_LEFT_COLLECT);
-                    rightIntake.setPower(HisaishiCalibration.INTAKE_RIGHT_COLLECT);
-                } else if (event.kind == EventKind.LEFT_TRIGGER_DOWN) {
-                    leftIntake.setPower(HisaishiCalibration.INTAKE_LEFT_DISPENSE);
-                    rightIntake.setPower(HisaishiCalibration.INTAKE_RIGHT_DISPENSE);
-                } else if (event.kind == EventKind.LEFT_TRIGGER_UP || event.kind == EventKind.LEFT_BUMPER_UP) {
-                    leftIntake.setPower(0.0);
-                    rightIntake.setPower(0.0);
+                switch (event.kind){
+                    case BUTTON_A_DOWN: //wheel intake
+                        leftIntake.setPower(HisaishiCalibration.INTAKE_LEFT_COLLECT);
+                        rightIntake.setPower(HisaishiCalibration.INTAKE_RIGHT_COLLECT);
+                        break;
+                    case BUTTON_B_DOWN:
+                        leftIntake.setPower(HisaishiCalibration.INTAKE_LEFT_DISPENSE);
+                        rightIntake.setPower(HisaishiCalibration.INTAKE_RIGHT_DISPENSE);
+                        break;
+                    case BUTTON_A_UP:
+                    case BUTTON_B_UP:
+                        leftIntake.setPower(0.0);
+                        rightIntake.setPower(0.0);
+                        break;
+                    case LEFT_BUMPER_DOWN: //claw
+                        claw.setPosition(HisaishiCalibration.CLAW_CLOSE);
+                        break;
+                    case LEFT_TRIGGER_DOWN:
+                        claw.setPosition(HisaishiCalibration.CLAW_OPEN);
+                        break;
+                    case BUTTON_X_DOWN: //lazy susan
+                        susan.setPosition(HisaishiCalibration.SUSAN_LEFT);
+                        break;
+                    case BUTTON_Y_DOWN:
+                        susan.setPosition(HisaishiCalibration.SUSAN_RIGHT);
+                        break;
+                }
+                }
+        });
+        this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1){
+            public void handleEvent(RobotEvent e){
+                GamepadEvent event = (GamepadEvent) e;
+                switch (event.kind) {
+                    case RIGHT_BUMPER_DOWN: //foundation arms
+                        leftArm.setPower(HisaishiCalibration.ARM_LEFT_STOW);
+                        rightArm.setPower(HisaishiCalibration.ARM_RIGHT_STOW);
+                        break;
+                    case RIGHT_TRIGGER_DOWN:
+                        leftArm.setPower(HisaishiCalibration.ARM_LEFT_DOWN);
+                        rightArm.setPower(HisaishiCalibration.ARM_RIGHT_DOWN);
+                        break;
+                    case RIGHT_TRIGGER_UP:
+                    case RIGHT_BUMPER_UP:
+                        leftArm.setPower(0.0);
+                        rightArm.setPower(0.0);
+                        break;
+                    case BUTTON_A_DOWN: //drivetrain
+                        driveTask.slowDown(false);
+                        break;
+                    case BUTTON_B_DOWN:
+                        driveTask.slowDown(true);
+                        break;
                 }
             }
         });
