ef9c23b5a16d396b7a5fa62f5e2743011da24b39
youracctname <yourname@rocknrollrobots25@gmail.com>
2019-12-17 19:34:28 -0800

    New Touch Sensor Test

---
+++ 2020/opmodes/REVTouchSensorCriteria.java
@@ -0,0 +1,21 @@
+package opmodes;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import team25core.SensorCriteria;
+
+public class REVTouchSensorCriteria implements SensorCriteria {
+    sensorTouch = HardwareMap.get(TouchSensor.class, "Touch Sensor");
+
+    
+        public REVTouchSensorCriteria(sensorTouch, ){
+
+
+        }
+
+    @Override
+    public boolean satisfied(){
+
+    }
+
+}
---
+++ 2020/opmodes/test/REVTouchSensorTest.java
@@ -0,0 +1,61 @@
+package opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import team25core.GamepadTask;
+import team25core.MechanumGearedDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import team25core.TankMechanumControlScheme;
+import team25core.TeleopDriveTask;
+
+@TeleOp(name = "REV TouchSensor Test")
+public class REVTouchSensorTest extends Robot {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private TeleopDriveTask driveTask;
+    private MechanumGearedDrivetrain drivetrain;
+
+    public void handleEvent(RobotEvent e) {
+
+    }
+
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        touchRight = hardwareMap.get(TouchSensor.class, "touchRight");
+        touchLeft = hardwareMap.get(TouchSensor.class, "touchLeft");
+
+        drivetrain = new MechanumGearedDrivetrain(60, frontRight, backRight, frontLeft, backLeft);
+        drivetrain.setNoncanonicalMotorDirection();
+        drivetrain.encodersOn();
+        drivetrain.resetEncoders();
+
+        TankMechanumControlScheme scheme = new TankMechanumControlScheme(gamepad1, TankMechanumControlScheme.MotorDirection.NONCANONICAL);
+
+        driveTask = new TeleopDriveTask(this, scheme, frontLeft, frontRight, backLeft, backRight);
+    }
+
+    @Override
+    public void start() {
+        this.addTask(driveTask);
+        this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1) {
+            public void handleEvent(RobotEvent e) {
+                GamepadEvent event = (GamepadEvent) e;
+                switch (event.kind) {
+                    case BUTTON_X_DOWN:
+
+
+
+                }
+            }
+        });
+    }
+}
